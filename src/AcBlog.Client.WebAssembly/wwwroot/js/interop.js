887859de594fba3d2b527ba428b438bf23355ed2
StardustDL <stardustdl@163.com>
2020-07-03 16:46:25 +0800

    update index page

--- src/AcBlog.Client.WebAssembly/wwwroot/js/interop.js
+++ src/AcBlog.Client.WebAssembly/wwwroot/js/interop.js
@@ -10,30 +10,12 @@ window.acblogInteropScrollTo = function (id) {
 window.acblogInteropCopyItem = function (ele) {
     ele.select();
     document.execCommand('copy');
-}
-window.acblogInteropModalAction = function (id, action) {
-    var m = $('#' + id);
-    if (m) {
-        m.modal(action);
-    }
-};
-window.acblogInteropToastAction = function (id, action) {
-    var m = $('#' + id);
-    if (m) {
-        m.modal(action);
-    }
-};
-window.acblogInteropTooltipEnable = function () {
-    $('[data-toggle="tooltip"]').tooltip();
-};
-window.acblogInteropPopoverEnable = function () {
-    $('[data-toggle="popover"]').popover()
 };
 window.acblogInteropLoadingInfoShow = function () {
     var loadingInfo = document.getElementById("loading-info-ui");
     loadingInfo.style = "display: block";
-}
+};
 window.acblogInteropLoadingInfoHide = function () {
     var loadingInfo = document.getElementById("loading-info-ui");
     loadingInfo.style = "display: none";
-}
+};
