55ebd6832d511ac31a36ad3ad2238e80a1275767
StardustDL <stardustdl@163.com>
2020-07-24 08:31:43 +0800

    add server-side blazor (no-auth)

---
+++ src/client/AcBlog.Client.Server/Startup.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using AcBlog.Client.Server.Areas.Identity;
+using AcBlog.Client.UI;
+using AcBlog.Client.Models;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace AcBlog.Client.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            services.AddSingleton(new RuntimeOptions { IsPrerender = false });
+            services.AddUIComponents();
+
+            services.AddClientConfigurations(Configuration);
+            {
+                var server = new ServerSettings();
+                Configuration.GetSection("Server").Bind(server);
+
+                static void AddServerAuthorization(IServiceCollection services, IdentityServerSettings identityProvider)
+                {
+                    if (identityProvider.Enable)
+                    {
+                        services.AddAuthentication();
+
+                        services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+                    }
+                    else
+                    {
+                        services.AddAuthorizationCore();
+                        services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+                        services.AddScoped<SignOutSessionStateManager>();
+                    }
+                }
+
+                AddServerAuthorization(services, server.Identity);
+            }
+
+            services.AddBlogService(Configuration.GetBaseAddress());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
---
+++ src/client/AcBlog.Client.Server/Utils.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AcBlog.Client.Server
+{
+    public static class Utils
+    {
+        public static async Task<TResult> WaitAsync<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+            using var timeoutCancellationTokenSource = new CancellationTokenSource();
+            var delayTask = Task.Delay(timeout, timeoutCancellationTokenSource.Token);
+            if (await Task.WhenAny(task, delayTask) == task)
+            {
+                timeoutCancellationTokenSource.Cancel();
+                return await task;
+            }
+            throw new TimeoutException("The operation has timed out.");
+        }
+
+        public static string GetBaseAddress(this IConfiguration configuration)
+        {
+            return configuration.GetValue<string>("BaseAddress");
+        }
+    }
+}
